390224edfa107a5588c84f33270be88c75e806e4
Arturo Cordero <vakmancordero@gmail.com>
2018-05-23 12:14:35 -0500

    Initializing repository

---
+++ src/main/java/com/kaizensoftware/cachingexample/CachingExampleApplication.java
@@ -0,0 +1,12 @@
+package com.kaizensoftware.cachingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingExampleApplication.class, args);
+	}
+}
